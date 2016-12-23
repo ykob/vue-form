514ec45044e670c69c30b2b2ff732f0932d9d556
yoichi kobayashi <info@tplh.net>
2016-12-23 23:52:52 +0900

    [WIP] create components.

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -1,5 +1,10 @@
 // import Preloader from '../modules/preloader.js';
 
 export default function() {
-
+  const vm = new Vue({
+    el: '#app',
+    components: {
+      msg: require('../vue/form.vue')
+    }
+  })
 };
---
+++ src/js/vue/form.vue
@@ -0,0 +1,20 @@
+<style lang="scss">
+  .red {
+    color: #f00;
+  }
+</style>
+
+<template lang="pug">
+  h1.red
+    |{{ msg }}
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello world!'
+      }
+    }
+  }
+</script>
