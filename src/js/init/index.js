504b948f33c2d854ac8ab09dcc888bfb78d7a308
yoichi kobayashi <info@tplh.net>
2017-01-05 00:56:48 +0900

    [remove] vue components.

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -1,10 +1,2 @@
-// import Preloader from '../modules/preloader.js';
-
 export default function() {
-  const vm = new Vue({
-    el: '#app',
-    components: {
-      msg: require('../vue/form.vue')
-    }
-  })
 };
