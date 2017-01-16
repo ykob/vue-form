3f09284c17be3acc241d184636a301a7e2324d9c
yoichi kobayashi <info@tplh.net>
2017-01-16 14:46:32 +0900

    [edit] value of gulp vendor script options.

--- gulp/conf.js
+++ gulp/conf.js
@@ -59,7 +59,7 @@ module.exports.vendorScripts = {
   src: [
     `./${DIR.SRC}/js/vendor/jquery.js`,
     `./${DIR.SRC}/js/vendor/jquery.easing.js`,
-    `./${DIR.SRC}/js/vendor/vue.js`,
+    `./${DIR.SRC}/js/vendor/vue.min.js`,
   ],
   concat: 'vendor.js',
   dest: `./${DIR.DEST}${DIR.PATH}/js/`
