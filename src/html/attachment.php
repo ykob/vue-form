557e68561e7045a44ff86a32d96468b3b9fcd9fe
yoichi kobayashi <info@tplh.net>
2017-09-07 12:03:21 +0900

    [add] new process file upload and send mail.

--- src/html/attachment.php
+++ src/html/attachment.php
@@ -17,5 +17,40 @@
   }
 
   // upload file.
-  move_uploaded_file($file['tmp_name'], $dir. $file['name']);
+  $filepath = $dir. $file['name'];
+  move_uploaded_file($file['tmp_name'], $filepath);
+
+  // send mail
+  $boundary = "__BOUNDARY__";
+
+  $additional_headers = "Content-Type: multipart/mixed; boundary=\"{$boundary}\"\n";
+  $additional_headers .= "Content-Transfer-Encoding: 8bit\n";
+  $additional_headers .= "From: info@tplh.net\n";
+
+  $message = "--{$boundary}\n";
+
+  $message .= "Content-Type: text/plain; charset=\"ISO-2022-JP\"\n";
+  $message .= "\n";
+  $message .= "これはメール本文です。\n";
+
+  $message .= "--{$boundary}\n";
+
+  $message .= "Content-Type: text/plain; charset=\"ISO-2022-JP\"\n";
+  $message .= "\n";
+  $message .= "これはメール本文です。2\n";
+
+  $message .= "--{$boundary}\n";
+
+  $filetype = mime_content_type($filepath);
+  $filename = basename($filepath);
+
+  $message .= "Content-Type: {$filetype}; name=\"{$filename}\"\n";
+  $message .= "Content-Disposition: attachment; filename=\"{$filename}\"\n";
+  $message .= "Content-Transfer-Encoding: base64\n";
+  $message .= "\n";
+  $message .= chunk_split(base64_encode(file_get_contents($filepath)))."\n";
+
+  $message .= "--{$boundary}--";
+
+  mb_send_mail('koba@tsumikiinc.com', '添付ファイルのテスト送信', $message, $additional_headers);
 ?>
